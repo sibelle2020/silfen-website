764abb593a66c34c3eb633cb8295fdb4fe94da48
Sandra Sibelle <sibellemusic@gmail.com>
2021-05-05 18:23:05 +0200

    front page styling

--- js/index.js
+++ js/index.js
@@ -1,23 +1,32 @@
 const urlParams = new URLSearchParams(window.location.search);
-let urlall = "https://s21kea-d06b.restdb.io/rest/silfen-products?sort=name";
+const url =
+  "https://s21kea-d06b.restdb.io/rest/silfen-products?sort=name" +
+  `&q={"type":"bestsellers"}` +
+  "&max=4";
 
 const options = {
-  method: "GET",
   headers: {
     "x-apikey": "6033bd605ad3610fb5bb64f6",
   },
 };
 
-fetch(urlall, options)
-  .then(function (res) {
-    return res.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    allBags(data);
-  });
+fetch(url, options)
+  .then((res) => res.json())
+  .then((data) => showBags(data));
 
-function allBags(data) {
-  console.log(data);
-  data.forEach(showBags);
+function showBags(data) {
+  data.forEach(showBest);
+}
+
+function showBest(bag) {
+  const template = document.querySelector("#best-template").content;
+  const clone = template.cloneNode(true);
+
+  clone.querySelector("img").src = bag.imgmodel;
+  clone.querySelector("h2").textContent = bag.name;
+  clone.querySelector("p").textContent = `${bag.price} DKK`;
+  clone.querySelector(".link-to-best").href = `product.html?id=${bag._id}`;
+
+  const parent = document.querySelector(".bags-wrapper-best");
+  parent.appendChild(clone);
 }
