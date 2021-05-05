1a53bc8149f05ce97c10ea75945893de81f992e1
Sandra Sibelle <sibellemusic@gmail.com>
2021-05-05 10:26:38 +0200

    dropdown filter buttons js fixed

--- js/productlist.js
+++ js/productlist.js
@@ -26,3 +26,54 @@ function allBags(data) {
   console.log(data);
   data.forEach(showBags);
 }
+
+function myFunction() {
+  document.querySelector("#catDropdown").classList.toggle("show");
+
+  window.onclick = function (event) {
+    if (!event.target.matches(".filter-dropbtn-category")) {
+      let dropdowns = document.querySelector(".dropdown-content-category");
+      let i;
+      for (i = 0; i < dropdowns.clientHeight; i++) {
+        let openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains("show")) {
+          openDropdown.classList.remove("show");
+        }
+      }
+    }
+  };
+}
+
+function myFunction2() {
+  document.querySelector("#colorDropdown").classList.toggle("show");
+
+  window.onclick = function (event) {
+    if (!event.target.matches(".filter-dropbtn-colors")) {
+      let dropdowns = document.querySelector(".dropdown-content-colors");
+      let i;
+      for (i = 0; i < dropdowns.clientHeight; i++) {
+        let openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains("show")) {
+          openDropdown.classList.remove("show");
+        }
+      }
+    }
+  };
+}
+
+function myFunction3() {
+  document.querySelector("#priceDropdown").classList.toggle("show");
+
+  window.onclick = function (event) {
+    if (!event.target.matches(".filter-dropbtn-price")) {
+      let dropdowns = document.querySelector(".dropdown-content-price");
+      let i;
+      for (i = 0; i < dropdowns.clientHeight; i++) {
+        let openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains("show")) {
+          openDropdown.classList.remove("show");
+        }
+      }
+    }
+  };
+}
